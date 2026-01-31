460e977a82123bb017e464c6189abfbd2cc9c372
Shreya <shreyamalli5@gmail.com>
2026-01-31 22:39:07 +0530

    Update cors in controllers

--- backend/src/main/java/com/coffie/coffie_ai_coach/controller/AiController.java
+++ backend/src/main/java/com/coffie/coffie_ai_coach/controller/AiController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://coffie-ai-coach-frontend.onrender.com/")
 @RestController
 @RequestMapping("/api/ai")
 public class AiController {
--- backend/src/main/java/com/coffie/coffie_ai_coach/controller/JournalController.java
+++ backend/src/main/java/com/coffie/coffie_ai_coach/controller/JournalController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://coffie-ai-coach-frontend.onrender.com/")
 @RestController
 @RequestMapping("/api/journal")
 public class JournalController {
