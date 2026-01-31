2df614c52100326980e13cae81e1f86ac0cf3e1f
Shreya <shreyamalli5@gmail.com>
2026-01-31 22:01:20 +0530

    Update url in axiosClient

--- frontend/journal-ui/src/api/axiosClient.js
+++ frontend/journal-ui/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://coffie-ai-coach.onrender.com/api",
     headers: {
         "Content-Type": "application/json"
     }
